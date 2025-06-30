5f6cbe0f3a86346838c68aabc9bd015cbf9d5df0
Gajanan Joshi <gajananjoshi0601@gmail.com>
2025-06-30 13:08:32 +0530

    Updated App and investment logic, added Results and UserInput components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+import { useState } from 'react';
 import Header from './components/Header.jsx';
+import Results from './components/Results.jsx';
+import UserInput from './components/UserInput.jsx';
 
 function App() {
-  
-  return (
-    <Header />
+      const [userInput, setUserInput] = useState({initialInvestment: 10000, annualInvestment: 1200, expectedReturn: 6, duration: 10,});
 
-    
+    const inputIsValid = userInput.duration >= 1;
+
+    function handleChange(inputIdentifier, newValue){
+        setUserInput((prevUserInput) => {
+            return{
+            ...prevUserInput,    //This is used to collect all the values that are prev. added in our useState
+            [inputIdentifier]: +newValue     //This is used to set a newValue that the user have been entered 
+            };
+        });
+    }
+
+  return (
+    <>
+    <Header />
+    <UserInput userInput={userInput} onChange={handleChange} />
+    {!inputIsValid && <p className="center">Please enter a duration greater than zero.</p> }
+    {inputIsValid && <Results input={userInput} />}
+  </>
   )
 }
 
